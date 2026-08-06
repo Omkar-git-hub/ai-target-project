1211f73496b721853317a017dbf120f3371a7443
Omkar.Nikam <omkarnikam.work@gmail.com>
2026-08-06 22:29:01 +0530

    feat: Add application constants

--- config.py
+++ config.py
@@ -8,13 +8,12 @@ class Config:
     def __init__(self):
         self.app_name = APP_NAME
         self.app_version = APP_VERSION
-        self.api_host = API_HOST
-        self.api_port = API_PORT
-        self.api_timeout = API_TIMEOUT
+        self.api_url = API_URL
+        self.api_key = API_KEY
         self.db_host = DB_HOST
         self.db_port = DB_PORT
         self.db_username = DB_USERNAME
         self.db_password = DB_PASSWORD
         self.db_name = DB_NAME
         self.log_level = LOG_LEVEL
-        self.log_format = LOG_FORMAT
+        self.log_file = LOG_FILE
--- constants.py
+++ constants.py
@@ -7,17 +7,16 @@ APP_NAME = "My Application"
 APP_VERSION = "1.0.0"
 
 # API constants
-API_HOST = "https://api.example.com"
-API_PORT = 443
-API_TIMEOUT = 30  # seconds
+API_URL = "https://api.example.com"
+API_KEY = "YOUR_API_KEY"
 
 # Database constants
 DB_HOST = "localhost"
 DB_PORT = 5432
-DB_USERNAME = "myuser"
-DB_PASSWORD = "mypassword"
+DB_USERNAME = "username"
+DB_PASSWORD = "password"
 DB_NAME = "mydatabase"
 
 # Logging constants
 LOG_LEVEL = "INFO"
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+LOG_FILE = "app.log"
