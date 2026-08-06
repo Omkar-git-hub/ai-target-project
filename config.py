3cd1a28eb975044b1e51e076b35c8f67101f3534
runner <runner@runnervmvrwv9>
2026-08-06 16:48:45 +0000

    feat: Add application constants

--- config.py
+++ config.py
@@ -2,19 +2,19 @@
 Application configuration.
 """
 
-import os
 from constants import *
 
 class Config:
     def __init__(self):
         self.app_name = APP_NAME
-        self.version = VERSION
-        self.api_url = API_URL
-        self.api_key = API_KEY
+        self.app_version = APP_VERSION
+        self.api_host = API_HOST
+        self.api_port = API_PORT
+        self.api_timeout = API_TIMEOUT
         self.db_host = DB_HOST
         self.db_port = DB_PORT
         self.db_username = DB_USERNAME
         self.db_password = DB_PASSWORD
         self.db_name = DB_NAME
         self.log_level = LOG_LEVEL
-        self.log_file = LOG_FILE
+        self.log_format = LOG_FORMAT
--- constants.py
+++ constants.py
@@ -4,19 +4,20 @@ Application constants.
 
 # General constants
 APP_NAME = "My Application"
-VERSION = "1.0.0"
+APP_VERSION = "1.0.0"
 
 # API constants
-API_URL = "https://api.example.com"
-API_KEY = "YOUR_API_KEY"
+API_HOST = "https://api.example.com"
+API_PORT = 443
+API_TIMEOUT = 30  # seconds
 
 # Database constants
 DB_HOST = "localhost"
 DB_PORT = 5432
-DB_USERNAME = "username"
-DB_PASSWORD = "password"
-DB_NAME = "my_database"
+DB_USERNAME = "myuser"
+DB_PASSWORD = "mypassword"
+DB_NAME = "mydatabase"
 
 # Logging constants
 LOG_LEVEL = "INFO"
-LOG_FILE = "app.log"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
